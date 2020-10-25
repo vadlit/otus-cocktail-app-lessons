bb53965d844cff18ac24f8795cc5bc9323942241
Vadim Litvinov <vadlit@vadlit.ru>
2020-10-25 23:59:52 +0300

    ValueNotifier

--- lesson_07/homework/lib/ui/widgets/search/view/category_bubble.dart
+++ lesson_07/homework/lib/ui/widgets/search/view/category_bubble.dart
@@ -18,21 +18,22 @@ class CategoryBubble extends StatefulWidget {
 }
 
 class _State extends State<CategoryBubble> {
+  ValueNotifier<bool> _isSelectedNotifier;
   final CocktailCategory _category;
   StreamSubscription<CocktailCategory> _selectedCategoryUpdates;
 
   _State(this._category) {
+    _isSelectedNotifier = ValueNotifier(SelectedCategoryProvider().category == _category);
     _selectedCategoryUpdates =
-        SelectedCategoryProvider().updatesStream().listen((event) {
-      setState(() {
-        // do nothing, just update
-      });
+        SelectedCategoryProvider().updatesStream().listen((selectedCategory) {
+      _isSelectedNotifier.value = selectedCategory == _category;
     });
   }
 
   @override
   void dispose() {
     _selectedCategoryUpdates.cancel();
+    _isSelectedNotifier.dispose();
     super.dispose();
   }
 
@@ -42,17 +43,21 @@ class _State extends State<CategoryBubble> {
         onTap: () {
           SelectedCategoryProvider().category = _category;
         },
-        child: Container(
-            padding: const EdgeInsets.fromLTRB(16, 14, 16, 14),
-            margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-            decoration: BoxDecoration(
-                border: Border.all(color: const Color.fromARGB(255, 45, 44, 57)),
-                color: isSelected
-                    ? const Color.fromARGB(255, 59, 57, 83)
-                    : const Color.fromARGB(255, 32, 31, 44),
-                borderRadius: const BorderRadius.all(const Radius.circular(30))),
-            child: Text(_category.value,
-                style: Theme.of(context).textTheme.subtitle2)));
+        child: ValueListenableBuilder(
+            valueListenable: _isSelectedNotifier,
+            builder: (_, isSelected, __) => Container(
+                padding: const EdgeInsets.fromLTRB(16, 14, 16, 14),
+                margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 45, 44, 57)),
+                    color: isSelected
+                        ? const Color.fromARGB(255, 59, 57, 83)
+                        : const Color.fromARGB(255, 32, 31, 44),
+                    borderRadius:
+                        const BorderRadius.all(const Radius.circular(30))),
+                child: Text(_category.value,
+                    style: Theme.of(context).textTheme.subtitle2))));
   }
 
   bool get isSelected => SelectedCategoryProvider().category == _category;
